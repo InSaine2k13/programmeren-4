8c5313be3b3def4c67d9d276a0678f668b5ad094
InSaine2k13 <mohamed.h@live.nl>
2018-12-03 18:26:59 +0100

    Create Error.test.js

---
+++ test/Error.test.js
@@ -0,0 +1,26 @@
+//
+// Unittest for the Game class
+//
+const chai = require('chai')
+const assert = require('assert')
+const Error = require('../src/models/error.model')
+
+chai.should()
+
+describe('Error', () => {
+
+    it('should be intitialized successfully when providing valid arguments', (done) => {
+        const error = new Error('  abc  ', '  1111  ')
+
+        error.should.have.property('message').that.is.a('string').which.equals('abc')
+        error.should.have.property('code').that.is.a('string').which.equals('1111')
+        error.should.have.property('date').that.is.a('date').which.equals(new Date())
+        error.message.should.be.a('string')
+        error.code.should.be.a('string')
+
+        assert.equal(error.name, 'abc', 'Names do not match')
+
+        done()
+    })
+
+})
