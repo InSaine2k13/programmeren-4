f4a610da4399b60101df6b5626b08643008e8631
InSaine2k13 <mohamed.h@live.nl>
2018-12-06 11:45:34 +0100

    Updated

--- src/routes/game.routes.js
+++ src/routes/game.routes.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const gameController = require('../controllers/game.controller')
 const authController = require('../controllers/auth.controller')
 
-router.get('/games', gameController.getAll)
-router.get('/games/:gameId', gameController.getById)
-router.post('/games', gameController.addNewGame)
+router.get('/games', authController.validateJWT, gameController.getAll)
+router.get('/games/:gameId', authController.validateJWT, gameController.getById)
+router.post('/games', authController.validateJWT, gameController.addNewGame)
 
 router.post('/register', authController.register)
 router.post('/login', authController.login)
